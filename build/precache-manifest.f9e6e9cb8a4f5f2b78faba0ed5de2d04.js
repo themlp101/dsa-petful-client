48f06582deb180570e23d9c1264498c31151ff30
Matt <63121708+themlp101@users.noreply.github.com>
2020-08-29 14:47:41 -0400

    deploying

---
+++ build/precache-manifest.f9e6e9cb8a4f5f2b78faba0ed5de2d04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10406ea262bec7ca26404b445a6a73f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "024c4358229f03c2d5dc",
+    "url": "/static/css/main.d3e4027c.chunk.css"
+  },
+  {
+    "revision": "1e07aa56a2339929e31f",
+    "url": "/static/js/2.e6585966.chunk.js"
+  },
+  {
+    "revision": "eb56b19eba63a53416f4fd778023529f",
+    "url": "/static/js/2.e6585966.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "024c4358229f03c2d5dc",
+    "url": "/static/js/main.54feb0fb.chunk.js"
+  },
+  {
+    "revision": "72c5965cbfb3ac6a55d7",
+    "url": "/static/js/runtime-main.40d8ffab.js"
+  }
+]);
